56c645c30f39b3c050788fc72b89fa7c2b59f73e
Chen <Andrew.Chen@windriver.com>
2020-06-15 09:05:41 -0700

    Link stock component to stocks

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,8 +3,8 @@
         <div class="panel panel-success">
             <div class="panel-heading">
                 <h3 class="panel-title">
-                    NAME
-                    <small>(Price: PRICE)</small>
+                    {{ stock.name }}
+                    <small>(Price: {{ stock.price }})</small>
                 </h3>
             </div>
             <div class="panel-body">
@@ -12,15 +12,40 @@
                     <input 
                         type="number"
                         class="form-control"
-                        placeholders="Quantity">
+                        placeholders="Quantity"
+                        v-model="quantity">
                 </div>
                 <div class="pull-right">
                     <button 
                         class="btn btn-success"
-                        >Buy</button>
+                        @click="buyStock"
+                        :disabled="quantity <= 0 || !Number.isInteger(Number(quantity))"
+                    >Buy</button>
                 </div>
                 
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(order);
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
